c079215d72a3b89bd61239108ed3a572bb84818c
052_arteddy bukit <89283796+arteddybukit@users.noreply.github.com>
2022-10-17 18:45:16 +0800

    Add files via upload

---
+++ PROJECT ORACLE JF/SECTION GREENFOOT/JF_V02_S03_L01PrjStudent/RobotWorld.java
@@ -0,0 +1,30 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public RobotWorld()
+    {
+        super(600, 400, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Robot robot =  new  Robot();
+        addObject(robot, 297, 371);
+    }
+}
